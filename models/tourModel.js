4f1263858ee05d98a995122c53d9d43c2a8c698f
luomi16 <254287020@qq.com>
2023-06-08 23:46:51 -0400

    Prevent duplicate reviews

--- models/tourModel.js
+++ models/tourModel.js
@@ -36,7 +36,8 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       default: 4.5,
       min: [1, 'Rating must be above 1.0'],
-      max: [5, 'Rating must be below 5.0']
+      max: [5, 'Rating must be below 5.0'],
+      set: val => Math.round(val * 10) / 10 //4.6666, 46.666, 47, 4.7
     },
     ratingsQuantity: {
       type: Number,
