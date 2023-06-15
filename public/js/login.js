06bd4cb78eebd7b9353e61174eb7636299635d6e
luomi16 <254287020@qq.com>
2023-06-14 21:39:20 -0400

    compression

--- public/js/login.js
+++ public/js/login.js
@@ -6,7 +6,7 @@ export const login = async (email, password) => {
   try {
     const res = await axios({
       method: 'POST',
-      url: 'http://127.0.0.1:3000/api/v1/users/login',
+      url: '/api/v1/users/login',
       data: {
         email,
         password
@@ -29,7 +29,7 @@ export const logout = async () => {
   try {
     const res = await axios({
       method: 'GET',
-      url: 'http://127.0.0.1:3000/api/v1/users/logout'
+      url: '/api/v1/users/logout'
     });
 
     if ((res.data.status = 'success')) {
--- public/js/updateSetting.js
+++ public/js/updateSetting.js
@@ -6,9 +6,7 @@ import { showAlert } from './alerts';
 export const updateSetting = async (data, type) => {
   try {
     const url =
-      type === 'password'
-        ? 'http://127.0.0.1:3000/api/v1/users/updateMyPassword'
-        : 'http://127.0.0.1:3000/api/v1/users/updateMe';
+      type === 'password' ? '/api/v1/users/updateMyPassword' : '/api/v1/users/updateMe';
 
     const res = await axios({
       method: 'PATCH',
